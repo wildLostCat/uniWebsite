45d995e8a37b2e4534d4144bbe38ebd83c9ae4ef
wildLostCat <wideopeneyess@proton.me>
2024-06-15 11:39:30 +0300

    somefixes

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+headers = ('name', 'city', 'cost')
+data = (('gazi', 'gaziantep', '40k'),
+        ('mine', 'gaziant', '30k'),
+        ('lod', 'gazntep', '40k'))
+
 @app.route('/')
 def table():
-    return render_template('table.html')
+    return render_template('table.html', headers=headers, data=data)
