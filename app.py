77607ac729c08b9ca38edb26fc2b369395f9769d
wildLostCat <wideopeneyess@proton.me>
2024-06-18 20:09:35 +0300

    update

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-headers = ('name', 'city', 'cost')
-data = (('gazi', 'gaziantep', '40k'),
-        ('mine', 'gaziant', '30k'),
-        ('lod', 'gazntep', '40k'))
+headers = ('name', 'city', 'cost', 'link')
+
+data = ((('gazi', 'gaziantep', '40k'), 'google.com'),
+        (('mine', 'gaziant', '30k'), 'youtube.com'),
+        (('lod', 'gazntep', '40k'), 'google.com'))
+
 
 @app.route('/')
 def table():
     return render_template('table.html', headers=headers, data=data)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
